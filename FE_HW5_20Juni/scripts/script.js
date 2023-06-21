aab31471b16e1ff1ae0c53dc093cfb5c8ea6adf5
AnnaTetjorin <135016640+AnnaTetjorin@users.noreply.github.com>
2023-06-21 21:26:47 +0600

    Frontend HW5 JS

---
+++ FE_HW5_20Juni/scripts/script.js
@@ -0,0 +1,28 @@
+/*const numValue = prompt("Write a number")
+console.log(numValue / 10)
+
+const firstNum = +prompt("Write a first number")
+const secondNum = +prompt("Write a second number")
+if (firstNum < secondNum) {
+    console.log(firstNum)
+} else {
+    console.log(secondNum)
+}
+
+const firstNum = +prompt("Write a number")
+if (firstNum < 100) {
+    console.log("Число меньше 100")
+} else
+if (firstNum > 100) {
+    console.log("Число больше 100")
+} else {
+    console.log("Число равно 100")
+}
+
+const userName = prompt("Введите своё имя")
+const numValue = +prompt("Введите свой возраст")
+if (numValue > 18) {
+    console.log(`Hello, ${userName}`)
+} else {
+    console.log(`Hi, ${userName}`)
+}*/
