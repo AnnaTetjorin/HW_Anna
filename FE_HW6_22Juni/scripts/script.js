71d6c8b3991f9febc329f8a0ff6c811092aad1f2
AnnaTetjorin <135016640+AnnaTetjorin@users.noreply.github.com>
2023-06-23 02:47:14 +0600

    FE HW6 22Juni

---
+++ FE_HW6_22Juni/scripts/script.js
@@ -0,0 +1,29 @@
+/*for(let i = 0; i <= 10; i = i + 2) {
+    console.log(i)
+}
+
+
+
+for(let i = 55; i >= 20; i = i - 1) {
+    console.log(i)
+}
+
+
+
+const numbers = [3, 5, 11, 2, 8, 1, 6]
+console.log(numbers)
+const numbers_squared = []
+numbers_squared.push(numbers[0]**2, numbers[1]**2, numbers[2]**2, numbers[3]**2, numbers[4]**2, numbers[5]**2, numbers[6]**2)
+console.log(numbers_squared)
+const last_elem = numbers_squared.pop()
+console.log(last_elem)
+
+
+
+const user = {
+    first_name: 'Ivan',
+    last_name: 'Ivanov',
+    age: 20,
+    salary: 500
+}
+console.log(`User's name is ${user.first_name} ${user.last_name}. He is ${user.age} years old`)*/
