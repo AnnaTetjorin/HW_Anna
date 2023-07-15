17438af6f96e8ce08619bb03ee28008569555df6
AnnaTetjorin <135016640+AnnaTetjorin@users.noreply.github.com>
2023-07-16 02:41:12 +0600

    BE HW 11.07.23

---
+++ BE_HW_11Juli/src/Main.java
@@ -0,0 +1,34 @@
+//Задание 1
+
+public class Main {
+    public static void main(String[] args) {
+       char a = 'G';
+       int b = 89;
+       byte c = 4;
+       short d = 56;
+       float e = 4.7333436f;
+       double f = 4.355453532;
+       long g = 12121l;
+
+        System.out.println("Character: " + a);
+        System.out.println("Integer: " + b);
+        System.out.println("Byte: " + c);
+        System.out.println("Short: " + d);
+        System.out.println("Float: " + e);
+        System.out.println("Double: " + f);
+        System.out.println("Long: " + g);
+
+
+        // Задание 2
+
+        String str = "345";
+        char s = str.charAt(0);
+        char v = str.charAt(1);
+        char m = str.charAt(2);
+
+        System.out.println("Число: " + str + "->" + s + "," + v + "," + m);
+    }
+}
+
+
+
