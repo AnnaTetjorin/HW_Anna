e50ff1da0af6d8bedcb8d1f7cfbe78ac07f719e0
AnnaTetjorin <135016640+AnnaTetjorin@users.noreply.github.com>
2023-09-19 04:38:24 +0600

    HW_13Sept_FLEX

---
+++ HW_13Sept/script.js
@@ -0,0 +1,20 @@
+/*НАЧАЛА ОТСЮДА ДЕЛАТЬ. НО ЗАПУТАЛАСЬ. БРОСИЛА:)
+СДЕЛАЛА ЧЕРЕЗ HTML CSS
+
+const theFirstElement = document.querySelector('#theFirst')
+
+const firstSection = document.createElement('section')
+firstSection.classList.add('first-section')
+theFirstElement.appendChild(firstSection)
+
+const h1Element = document.createElement('h1')
+h1Element.style.color = 'white'
+document.createTextNode('РЕАЛИЗУЕМ КРУПНЕЙШИЕ ПРОЕКТЫ В МИРЕ')
+heading.appendChild(textInsideHeader)
+h1Element.style.fontSize = '60px'
+
+const pElement = document.createElement('p')
+pElement.style.color = 'white'
+document.createTextNode('стадионы, газопроводы, мосты, дамбы')
+heading.appendChild(textInsideHeader)
+pElement.style.fontSize = '20px'*/
