80b922fad1f75fb9b2ce6037ff3789bc955fb6dc
AnnaTetjorin <135016640+AnnaTetjorin@users.noreply.github.com>
2023-10-26 03:20:17 +0600

    ALG_HW сортировка слиянием

---
+++ ALG_HW_сортировка слиянием/script.js
@@ -0,0 +1,30 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2)
+    const left = arr.slice(0, middle)
+    const right = arr.slice(middle)
+
+    return mergeSort(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right) {
+    let result = []
+    let leftIndex = 0
+    let rightIndex = 0
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex])
+            leftIndex++
+        }
+    }
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex))
+}
+
+
+const myArray = [38, 27, 43,3, 9, 82, 10]
+const sortedArray = mergeSort(myArray)
+console.log(sortedArray)
