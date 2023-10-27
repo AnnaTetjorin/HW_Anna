433cb771ba506d59484b251d5ea72e3a13f7d5c8
AnnaTetjorin <135016640+AnnaTetjorin@users.noreply.github.com>
2023-10-27 20:15:32 +0600

    ALG_HW_quickSort

---
+++ ALG_HW_QuickSort/script.js
@@ -0,0 +1,22 @@
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[Math.floor(arr.length / 2)];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else if (arr[i] > pivot) {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+const myArray = [3, 6, 8, 10, 1, 2, 1];
+const sortedArray = quickSort(myArray);
+console.log(sortedArray);
